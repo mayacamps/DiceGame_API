9bdd512ed272cf2c10d2c76ff6e76aab79a9cb8d
Maya <maya.camps@estudiantat.upc.edu>
2024-02-14 23:42:33 +0100

    updated validation messages

--- src/main/java/com/itacademy/diceGame/model/dto/request/PlayerDtoRequest.java
+++ src/main/java/com/itacademy/diceGame/model/dto/request/PlayerDtoRequest.java
@@ -1,18 +1,14 @@
 package com.itacademy.diceGame.model.dto.request;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.apache.commons.text.WordUtils;
-
-import java.util.Objects;
 
 @Data
 @NoArgsConstructor
 public class PlayerDtoRequest {
-    @NotNull(message = "cannot be null")
+    @NotBlank(message = "Please enter a name.")
     @Size(min = 2, max = 20)
     private String name;
 }
