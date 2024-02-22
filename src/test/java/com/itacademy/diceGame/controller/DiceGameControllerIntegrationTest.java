827bace88f1d417929f2adfed8d183f8bfe29d52
Maya <maya.camps@estudiantat.upc.edu>
2024-02-22 01:43:17 +0100

    started integration test

--- src/test/java/com/itacademy/diceGame/controller/DiceGameControllerIntegrationTest.java
+++ src/test/java/com/itacademy/diceGame/controller/DiceGameControllerIntegrationTest.java
@@ -29,5 +29,37 @@ public class DiceGameControllerIntegrationTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Test
+    @DisplayName("DiceGameControllerIntegrationTest - Test return Player list")
+    void whenGetPlayers_thenReturnStatusOKAndPLayerList() throws Exception {
+        mvc.perform(get("/api/v1/dicegame/players/")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(4)));
+    }
 
+    @Test
+    @DisplayName("DiceGameControllerIntegrationTest - Test create new Player")
+    void whenCreatePlayer_thenReturnStatusCreated() throws Exception {
+        PlayerDtoRequest newPlayer = new PlayerDtoRequest("alexia");
+        mvc.perform(post("/api/v1/dicegame/players/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newPlayer)))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$").value("Player successfully added with name: " + newPlayer.getName()));
+    }
+
+    @Test
+    @DisplayName("DiceGameControllerIntegrationTest - Test create new Player throws PlayerAlreadyExistsException")
+    void whenCreatePlayer_thenReturnStatusConflictAndException() throws Exception {
+        PlayerDtoRequest newPlayer = new PlayerDtoRequest("Lola");
+        mvc.perform(post("/api/v1/dicegame/players/")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(newPlayer)))
+                .andDo(print())
+                .andExpect(status().isConflict())
+                .andExpect(jsonPath("$").value("Player already exists with given name: " + newPlayer.getName()));
+    }
 }
